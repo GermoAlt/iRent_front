b6e88de75a283ac9f3f2491c5cddab6edf42d747
German <germanaltairac@hotmail.com>
2021-10-27 21:52:04 -0300

    refactor

--- src/containers/mainContent/MainContent.js
+++ src/containers/mainContent/MainContent.js
@@ -4,9 +4,7 @@ import {Redirect} from "react-router";
 import HomePage from "./homePage/HomePage";
 import TransactionLog from "./adminPanel/TransactionLog";
 import ProductPanel from "./adminPanel/ProductPanel";
-import ListaArticulos from "./articulos/itemList/ListaArticulos";
 import Carrito from "./cart/Carrito";
-import Checkout from "./checkout/Checkout";
 import ProductDetails from "./articulos/productDetails/ProductDetails";
 import UserProfile from "./userProfile/UserProfile";
 
@@ -15,13 +13,10 @@ export default function MainContent(props){
    <div className={"mainContent"}>
        <Switch>
            <Route exact path="/" component={HomePage}/>
-           <Route path="/articulos/:category" component={ListaArticulos}/>
            <Route path="/transacciones" component={TransactionLog}/>
            <Route path="/gestionProductos" component={ProductPanel}/>
            <Route path="/carrito" component={Carrito}/>
            <Route path="/producto/:id"component={ProductDetails}/>
-           <Route path="/carrito" component={Carrito}/>
-           <Route path="/checkout" component={Checkout}/>
            <Route path="/perfil" component={UserProfile}/>
 
            <Redirect to={"/"} />
---
+++ src/containers/mainContent/articulos/newArticle/newArticle.js
@@ -0,0 +1,17 @@
+import React, {useState} from "react";
+import "newArticle.css"
+
+export default function newArticle(){
+    //Aca van las variables
+    const [ precio, setPrecio ] = useState(0);
+
+
+
+    return (
+        <div className={"new-article-container"}>
+            <!-- ACA VAN LOS COMPONENTES -->
+        </div>
+    )
+}
+
+//Aca van las funciones auxiliares
